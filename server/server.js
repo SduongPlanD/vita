d1048c84f91d2774383a168ba5747877f9658440
Steve Duong <steve@Plan.D>
2020-03-02 19:53:28 -0800

    adds server side

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const path = require('path');
+const app = express();
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/api/hello', (req, res) => {
+  res.send({ express: 'Hello From Express' });
+});
+
+app.post('/api/world', (req, res) => {
+  console.log(req.body);
+  res.send(
+    `I received your POST request. This is what you sent me: ${req.body.post}`,
+  );
+});
+
+app.listen(process.env.PORT || 8080);
--- src/Components/App.js
+++ src/Components/App.js
@@ -10,8 +10,40 @@ import './App.css';
 
 class App extends Component {
   state = {
-    sideDrawerOpen: false
+    sideDrawerOpen: false,
+    tempData: 'blank',
+    response: '',
+    post: '',
+    responseToPost: '',
   }
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ response: res.express }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/hello');
+    const body = await response.json();
+    if (response.status !== 200) throw Error(body.message);
+
+    return body;
+  };
+
+  handleSubmit = async e => {
+    e.preventDefault();
+    const response = await fetch('/api/world', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ post: this.state.post }),
+    });
+    const body = await response.text();
+
+    this.setState({ responseToPost: body });
+  };
+
 
   toggleDrawerClickHandler = () => {
     this.setState((prevState) => {
@@ -38,7 +70,7 @@ class App extends Component {
         {backDrop}
         < HomePage ></HomePage>
         <MealLibrary />
-        <p>the is the info on current page</p>
+        <p>{this.state.response}</p>
       </div>
     );
   }
